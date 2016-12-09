440d951af107f94c21117c27ee6cad6efa139a41
Olle Svensson <olle@kent.nu>
2016-12-09 21:08:04 +0100

    Minor fixes (set max page, margin-bottom on content)

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,40 +4,37 @@
 
 var loading = '<p>Loading...</p>'
 var finished = '<p>The End.</p>'
-var isFinished = false
+var maxPage = undefined
 
 var init = function () {
 
   // Dynamic sizing of iframes
   refreshFitVids();
+
   // Infinite scrolling
   $('#wrapper').infinitescroll({
     navSelector  : '#nav',
     nextSelector : '#next',
     itemSelector : '#wrapper div.post-body',
     loading: {
-      finished : appendFinished,
-      finishedMsg : null,
+      finished : undefined,
+      finishedMsg : finished,
       msg : null,
       msgText : loading,
       selector : '#info-loading',
     },
-    bufferPx : 200
+    bufferPx : 200,
+    maxPage : maxPage
   }, refreshFitVids);
 
 }
 
-var refreshFitVids = function () {
-  $('#content').fitVids();
+var setMaxPage = function (max) {
+  maxPage = parseInt(max);
 }
 
-var appendFinished = function () {
-
-  // Infinite scroll sometimes reaches finished twice
-  if (isFinished) { return; }
-
-  isFinished = true;
-  $('#info-loading').append(finished);
+var refreshFitVids = function () {
+  $('#content').fitVids();
 }
 
 // Init everything
