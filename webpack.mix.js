a9aa75efa6203e3a2bbc3fcc6d6f3a172d6cc09f
Olle <me@olle.io>
2019-11-20 20:57:35 +0100

    wip: add tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,7 @@
 let mix = require('laravel-mix');
+let tailwindcss = require('tailwindcss');
+let glob = require("glob-all");
+let PurgecssPlugin = require("purgecss-webpack-plugin");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +14,45 @@ let mix = require('laravel-mix');
  |
  */
 
+// Custom PurgeCSS extractor for Tailwind that allows special characters in
+// class names.
+//
+// https://github.com/FullHuman/purgecss#extractor
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
+// Only run PurgeCSS during production builds for faster development builds
+// and so you still have the full set of utilities available during
+// development.
+if (mix.inProduction()) {
+  mix.webpackConfig({
+    plugins: [
+      new PurgecssPlugin({
+
+        // Specify the locations of any files you want to scan for class names.
+        paths: glob.sync([
+          path.join(__dirname, "*.hbs"),
+          path.join(__dirname, "partials/*.hbs"),
+        ]),
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+
+            // Specify the file extensions to include when scanning for
+            // class names.
+            extensions: ["hbs", "js", "ts"]
+          }
+        ]
+      })
+    ]
+  });
+}
+
 mix.options({
+    postCss: [ tailwindcss('tailwind.config.js') ],
     processCssUrls: false,
   })
   .copyDirectory('src/fonts', 'assets/fonts')
