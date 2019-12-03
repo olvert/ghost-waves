e099a4eb8cd23851c6bb63e149811b6c55847ce8
Olle <me@olle.io>
2019-12-03 21:48:24 +0100

    rewrite css with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,33 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        grayish: '#999999',
+        blackish: '#333333',
+      },
+      fontFamily: {
+        futura: [
+          'Futura',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+      fontSize: {
+        xxs: '0.75rem',
+        xs: '0.8125rem',
+      },
+    },
   },
   variants: {},
-  plugins: []
-}
+  plugins: [],
+};
